19443712050a595a637e9d350e07cf79efdfdb0f
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-06 01:48:21 +0000

    [Bookers] create時のvaridatesとerrormessageの定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
